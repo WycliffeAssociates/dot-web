fc6b64ef5dee0b5eb2dfba4169b63668da445f1c
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-06-13 14:54:18 -0500

    removed shadow on chapters track

--- src/components/PlayerNavigation/ChaptersList.tsx
+++ src/components/PlayerNavigation/ChaptersList.tsx
@@ -13,9 +13,7 @@ export function ChapterList(props: IChapterList) {
     <Show when={props.currentVid}>
       <ul
         data-js="chapterButtonTrack"
-        class={`flex flex-nowrap gap-3 items-start content-start px-2 py-4 overflow-x-auto scrollbar-hide  list-none scroll-smooth motion-reduce:scroll-auto w-full	 ${
-          props.showChapSliderButtons() ? "x-scroll-gradient" : ""
-        }`}
+        class={`flex flex-nowrap gap-3 items-start content-start px-2 py-4 overflow-x-auto scrollbar-hide  list-none scroll-smooth motion-reduce:scroll-auto w-full`}
       >
         <For each={currentBook()}>
           {(vid) => {
